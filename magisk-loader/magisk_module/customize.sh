76ce64177d72ffcd2d4e42dc07dc06fcb437b7cc
Infiniti151 <43163551+Infiniti151@users.noreply.github.com>
2026-04-12 18:43:02 +0530

    fix action button logic

--- magisk-loader/magisk_module/customize.sh
+++ magisk-loader/magisk_module/customize.sh
@@ -79,6 +79,7 @@ fi
 ui_print "- Extracting module files"
 
 extract "$ZIPFILE" 'module.prop'        "$MODPATH"
+extract "$ZIPFILE" 'action.sh'          "$MODPATH"
 extract "$ZIPFILE" 'post-fs-data.sh'    "$MODPATH"
 extract "$ZIPFILE" 'service.sh'         "$MODPATH"
 extract "$ZIPFILE" 'uninstall.sh'       "$MODPATH"
@@ -186,6 +187,7 @@ set_perm_recursive "$MODPATH" 0 0 0755 0644
 set_perm_recursive "$MODPATH/bin" 0 2000 0755 0755 u:object_r:magisk_file:s0
 set_perm_recursive "/data/adb/lspd/" 0 0 0755 0644
 set_perm_recursive "/data/adb/lspd/bin" 0 0 0755 0755 u:object_r:magisk_file:s0
+set_perm "$MODPATH/action.sh" 0 0 0755 u:object_r:magisk_file:s0
 chmod 0744 "$MODPATH/daemon"
 chmod 0700 "/data/adb/lspd/bin/cli"
 
